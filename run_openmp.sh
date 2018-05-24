6b38c713b895618bc10dd1514f8adbcbe197bc70
andfsanchezlem <andfsanchezlem@unal.edu.co>
2018-05-24 04:52:50 -0500

    other

---
+++ run_openmp.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+for kernel in 3 5 7 9 11 13 15
+do
+    for n_threads in 1 2 4 8
+    do
+        echo "------------------------------------------" >> mediciones_openmpi.txt
+        echo "Imagen en 1080" >> mediciones_openmpi.txt
+        echo "Usando un kernel de " $kernel "y" $n_threads "hilos">> mediciones_openmpi.txt
+        { time mpirun -np $n_threads --hostfile hostfile mpi_blur_effect 480.jpg $kernel >/dev/null 2>&1; } 2>> mediciones_openmpi.txt
+
+        echo "------------------------------------------" >> mediciones_openmpi.txt
+        echo "Imagen en 720" >> mediciones_openmpi.txt
+        echo "Usando un kernel de " $kernel "y" $n_threads "hilos">> mediciones_openmpi.txt
+        { time mpirun -np $n_threads --hostfile hostfile mpi_blur_effect 480.jpg $kernel >/dev/null 2>&1; } 2>> mediciones_openmpi.txt
+
+        echo "------------------------------------------" >> mediciones_openmpi.txt
+        echo "Imagen en 480" >> mediciones_openmpi.txt
+        echo "Usando un kernel de " $kernel "y" $n_threads "hilos">> mediciones_openmpi.txt
+        { time mpirun -np $n_threads --hostfile hostfile mpi_blur_effect 480.jpg $kernel >/dev/null 2>&1; } 2>> mediciones_openmpi.txt
+    done
+done
